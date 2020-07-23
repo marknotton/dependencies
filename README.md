fcd82562947ab6fa46ac92bd4d158b3c05d2fa15
Mark Notton <mark@marknotton.uk>
2020-07-23 11:34:49 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,144 @@
+# Dependencies for NPM
+
+  
+
+![Made For NPM](https://img.shields.io/badge/Made%20for-NPM-orange.svg)
+
+ Instantiate all NPM packages into a single object and sanitise their names.
+  
+
+## Installation
+
+```
+npm i @marknotton/dependencies --save-dev
+```
+
+```js
+const dependencies =  require('@marknotton/dependencies');
+```
+
+## But why?
+ 
+
+Code splitting tasks is ideal for maintaining your code in a coherent way, but in some cases it's useful to use the same instance of a node modules without re-requested them within each task file. This is actually compulsory for things like Gulp, as it uses callbacks after each task completes which can loose it's scope if you attempt to redefine it more than once in multiple tasks.
+
+This module will run through all the devDependencies from the package.json, Sanitise their names, scope their vendors, and initialise them as getters.
+
+*Please note that this tool is tailored specifically to my needs and may not be suitable for everyone. If you think this package should be expanded with additional options let me know.*
+
+### How names are sanitised:  
+
+Scoped packages (normally starting with '@') will be nested into their vendor name.
+
+Packages with the same module name as their vendor will still be nested (`dependencies.rollup.rollup`)
+
+All names will lose their 'gulp-', 'postcss-', 'plugin-', and 'rollup-' prefixes and will be camel cased.
+
+Some modules may have their names aliased into something else ('lumberjack' to 'log'), this is defined bespokely in this file.
+
+Reserved Javascript names will not be sanitised.  
+
+### Usage
+
+#### Old method:  
+
+```js
+const dotenv = require("dotenv"),
+	@doggistyle = require("@doggistyle/library"),
+	lumberjack = require("@marknotton/lumberjack"),
+	notifier = require("@marknotton/notifier"),
+	svgToSymbols = require("@marknotton/svg-to-symbols"),
+	autoprefixer = require("autoprefixer"),
+	browserSync = require("browser-sync"),
+	gulp = require("gulp"),
+	concat = require("gulp-concat"),
+	gulpif = require("gulp-if"),
+	plumber = require("gulp-plumber"),
+	postcss = require("gulp-postcss"),
+	rename = require("gulp-rename"),
+	terser = require("gulp-terser"),
+	postcssCustomProperties = require("postcss-custom-properties"),
+	rollup = require("rollup"),
+	rollupPluginCommonjs = require("@rollup/plugin-commonjs"),
+	rollupPluginMultiEntry = require("@rollup/plugin-multi-entry"),
+	rollupPluginNodeResolve = require("@rollup/plugin-node-resolve"),
+	postcssAssets = require("postcss-assets"),
+	snowpack = require("snowpack")
+```
+
+
+#### New method:  
+
+```js
+const dependencies = require('./gulp/dependencies')
+  
+const {
+	doggistyle : { core, library },
+	marknotton : { log, notifier, svgToSymbols },
+	postcss : { customProperties, assets },
+	rollup : { rollup, commonjs, multiEntry, nodeResolve },
+	dotenv, browserSync, gulp, concat, if, plumber, rename, terser, snowpack
+} = dependencies
+```
+
+
+**Remember this is not about syntactic sugar, this about retaining the scope of all the packages across all my tasks by only instantiating them once.**
+
+### Aliases 
+
+Some packages may need a bespoke reference to avoid conflicts or simply to sanitise the name to something that's easier to use. 'aliases' in your package.json
+
+file should have the original name as the key, and the new desired name as the value.
+
+It's possible to break the scoped vendor nesting by setting aliases if necessary. 
+
+```json
+"aliases": {
+  "@marknotton/lumberjack": "log",
+  "gulp-run-command": "run",
+  "gulp-if": "gulpif",
+  "vinyl-source-stream": "source",
+  "@rollup/stream": "rollup",
+  "@rollup/plugin-node-resolve": "@rollup/resolve",
+  "@rollup/plugin-multi-entry": "@rollup/multi"
+}
+```
+
+## Usage
+
+```js
+gulp.task('someTask', () => {
+return  gulp.src([...])
+.pipe(plumber({errorHandler: notifier.error }))
+.pipe(concat('ccombined.js'))
+.pipe(gulp.dest('/some/location/'))
+.pipe(notifier.success())
+});
+
+```
+
+## Options
+
+`log` `(Bool)` *( default: true )* - An output of all the package aliases and how to destructure the export will be logged. *Only works in 'dev' environments.* 
+
+`scope` `(Bool)` *( default: true )* - Scoped vendors will be nested in the final export. If false, the export will be flattened but may cause naming conflicts.
+
+`moduleHandler`  `(Function)`*(defaults : @see "Request Module" in this modules index.js)* - Handle special changes to certain dependencies; like instantiating a modules sub-method with a custom settings. 
+
+`nameTruncators`  `(Array)` - *( default : ['gulp-', 'postcss-', 'plugin-', 'rollup-'] )* =  Remove parts of the common vendor names to help sanitise the names how you'd like. You can completely edit the name using the "aliases" settings in your package.json or as another option *(see below)*. 
+
+`aliases`  `(Object)` - This serves the same functionality as the aliases setting describe further up this page. Defining the aliases in in this option will prioritised over the package.json settings. 
+
+```js
+const dependencies =  require('@marknotton/dependencies')({ 
+scope : false, 
+log : false,
+moduleHandler : module => {
+	if ( module == 'someModule' ) {
+		return require(module).default({...})
+	}
+},
+nameTruncators : [ 'webpack-' ],
+aliases : { "vinyl-source-stream": "source" }
+});
+```
---
+++ index.js
@@ -0,0 +1,215 @@
+////////////////////////////////////////////////////////////////////////////////
+// Node Dependencies 
+////////////////////////////////////////////////////////////////////////////////
+
+'use strict';
+
+// If this is true an output of all the package alias and how to destructure 
+// the export will be shown
+let showLog = true
+
+// If this is true, scroped vendors will be nested in the final export. 
+// If false, the export will be flattened but may cause naming conflicts.
+let scopeVendors = true
+
+// Loads environment variables from a .env file into process.env.
+require('dotenv').config()
+
+const log = require('@marknotton/lumberjack')
+
+const { name, aliases, devDependencies } = require('../../package.json')
+
+let _modules = {}
+let _vendors = []
+let _beforeAndAfterLogs = []
+let _nameTruncators = ['gulp-', 'postcss-', 'plugin-', 'rollup-']
+let _custonAliases = false
+let _moduleHandler = false
+
+// Add the project name as a process.env global constant and title case it.
+process.env.PROJECT = (() => {
+  return name ? name.replace('-', ' ') : 'Project'; 
+})().toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+
+function init() {
+
+  if ( devDependencies && Object.keys(devDependencies).length ) {
+    
+    //  Use custom aliases over any package.ons aliases
+    _custonAliases = _custonAliases ? _custonAliases : aliases
+
+    // Remove this module. It doesn't need to be used any further after it's innitial instantiation
+    if (devDependencies.hasOwnProperty('@marknotton/dependencies')) { delete devDependencies['@marknotton/dependencies'] }
+    
+    // Run through all the depenencies from the package.json relative to this folder
+    // and render each package into a modules object
+    Object.keys(devDependencies).forEach((module) => {
+
+      // Clone the name so that we can modify
+      let name = module
+      
+      // Check if there were any preferences to aliases modules
+      name = _custonAliases && _custonAliases.hasOwnProperty(name) ? _custonAliases[name] : name
+
+      // Check if this module has any special rules that justifty nesting it into 
+      // a nested item in the modules oject. The vendor name helps retain scope.
+
+      if ( scopeVendors  ) {
+        var vendor = (() => {
+          if ( name.includes('/') ) {
+            return name.split(/[\/ ]+/)[0].replace('@', '')
+          } else if ( name.includes('postcss-') || name == 'autoprefixer') {
+            return 'postcss'
+          } else if ( name.includes('rollup')  ) {
+            return 'rollup'
+          }
+        })()
+
+        if ( vendor && !_modules.hasOwnProperty(vendor)) { 
+          _modules[vendor] = []
+          _vendors.push(vendor)
+        }
+      }
+
+      // Split the name delimited by any forward slasses and return the last item.
+      name = name.split(/[\/ ]+/).pop()
+
+      // Remove common prefixes
+      _nameTruncators.forEach(modifier => {
+        name = name.replace(modifier, '')
+      })
+
+      // Javascript reserved words can't be used as a reference and will be
+      // prefixed with 'gulp-'
+      if ((/\b(abstract|arguments|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield)\b/g.test(name))) {
+        name = module
+      } 
+
+      // Camelcase the name
+      name = name.replace(/-(\w)/g, (m, part) => part.toUpperCase())
+
+      // Define the package into the plugin object as a getter
+      let reference = scopeVendors && vendor ? _modules[vendor] : _modules
+
+      _beforeAndAfterLogs.push([module, '=>', (scopeVendors && vendor ? `${vendor}.${name}` : name), ['green', '#11A8CD', 'yellow'], false, false])
+
+      Object.defineProperty(reference, name, {
+        enumerable: true,
+        get: () => requestModule(module)
+      })
+    })
+
+    if ( showLog && process.env.ENVIRONMENT == 'dev' ) {
+      renderBeforeAndAfter()
+      renderUsageOutput()
+    }
+  }
+
+  return _modules
+
+}
+// Request Module ==============================================================
+// There may be special circomstances where you want to instantiate a specific 
+// method inside a module. This function gives you that added control
+
+function requestModule(module) {
+
+  if ( _moduleHandler ) {
+    let customHandler = _moduleHandler(module)
+    if ( typeof customHandler !== 'undefined' ) {
+      return customHandler
+    }
+  }
+
+  switch (module) {
+    case 'browser-sync':
+      return require(module).create()
+    break;
+    case 'gulp-run-command':
+      return require(module).default
+    break;
+    case 'rollup':
+      return require(module).rollup
+    break;
+    case 'rollup-plugin-terser':
+      return require(module).terser
+    break;
+    case '@rollup/plugin-node-resolve':
+      return require(module).nodeResolve
+    break;
+    case 'dotenv':
+      return require(module).config()
+    break;
+    case 'postcss-assets':
+      const { paths } = require('../config.json')
+      return require(module)({loadPaths:[paths.images]})
+    break;
+    default:
+      return require(module)
+  }
+}
+
+// Render Before and After =====================================================
+// Log a list of all the packages that were requested and their alaises.
+
+function renderBeforeAndAfter() {
+  if ( _beforeAndAfterLogs.length ) {
+    
+    log(`${Object.values(devDependencies).length} packages have been loaded and will be exported from this module.`, ['yellow'], false, false)
+    log(`Their original references have been sanatised to the following aliases:`, ['yellow'], false, false)
+    log("==============================================================================", ['#464A50'], false, false)
+
+    _beforeAndAfterLogs.forEach(message => log(...message))
+  } else {
+    log('No packages were loaded. Check your packages.json files for devDependencies', [red])
+  }
+}
+
+// Render Usage Output =========================================================
+// Log an example of how to destructure all the packages 
+
+function renderUsageOutput() {
+  
+  log("==============================================================================", ['#464A50'], false, false)
+  log('You can destructure all your dependencies using the following syntax:', ['yellow'], false, false)
+  log("==============================================================================", ['#464A50'], false, false)
+  
+  let output = `const {`;
+
+  if ( scopeVendors ) {
+    for (const vendor of Object.values(_vendors)) {
+      output = `${output}
+    ${vendor} : { ${Object.keys(_modules[vendor]).join(', ')} },`
+    }
+  }
+  
+  output = `  ${output} 
+    ${Object.keys(_modules).filter(vendor => !_vendors.includes(vendor)).join(', ') }
+  } = require('@marknotton/dependencies')()`
+
+  log(output, ['#11A8CD'], false, false)
+  log("==============================================================================", ['#464A50'], false, false)
+  log("It's recommended that you only extract what you need, not everything.", ['yellow'], false, false)
+
+} 
+
+module.exports = (options) => {
+  if ( options ) {
+    if ( options.hasOwnProperty('log') ) {
+      showLog = options.log
+    }
+    if ( options.hasOwnProperty('scope') ) {
+      scopeVendors = options.scope
+    }
+    if ( options.hasOwnProperty('moduleHandler') ) {
+      _moduleHandler = options.moduleHandler
+    }
+    if ( options.hasOwnProperty('nameTruncators') ) {
+      _nameTruncators = options.nameTruncators
+    }
+    if ( options.hasOwnProperty('aliases') ) {
+      _custonAliases = options.aliases
+    }
+  }
+  return init()
+}
